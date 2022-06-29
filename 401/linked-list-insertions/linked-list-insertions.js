3c31a42b743a2daae4d39152a886097c1a303794
schillerandrew <schiller.andrew@gmail.com>
2022-06-28 22:52:37 -0700

    created insertBefore method

--- 401/linked-list-insertions/linked-list-insertions.js
+++ 401/linked-list-insertions/linked-list-insertions.js
@@ -1,49 +1,75 @@
 'use strict';
 
-class Node{
-  constructor(value){
+class Node {
+  constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
 
-class LinkedList{
-  constructor(){
+class LinkedList {
+  constructor() {
     this.head = null;
   }
 
-  append(newValue){
+  append(newValue) {
     const newNode = new Node(newValue);
-    if(!this.head){
+    if (!this.head) {
       this.head = newNode;
-      return;
+      return 'No list found';
     }
 
     let current = this.head;
 
-    while(current.next){
+    while (current.next) {
       current = current.next;
     }
-    current.next = node;
+    current.next = newNode;
   }
 
-  insertAfter(findThisValue, newValue){
+  insertBefore(findThisValue, newValue) {
     const newNode = new Node(newValue);
-    if(!this.head){
-      this.head = newNode;
-      return;
+    if (!this.head) {
+      return 'No match found';
+    }
+
+    let current = this.head;
+    let previous = null;
+
+    while (current) {
+      if (current !== findThisValue) {
+        previous = current;
+        current = current.next;
+      } else if (current === findThisValue && current === this.head)  {
+        newNode = this.head;
+        newNode.next = current;
+        return 'New node inserted';
+      } else if (current === findThisValue) {
+        previous.next = newNode;
+        newNode.next = current;
+        return 'New node inserted';
+      }
+    }
+    return 'No match found';
+  }
+
+  insertAfter(findThisValue, newValue) {
+    const newNode = new Node(newValue);
+    if (!this.head) {
+      return 'No match found';
     }
 
     let current = this.head;
 
-    while(current){
-      if(current !== findThisValue){
+    while (current) {
+      if (current !== findThisValue) {
         current = current.next;
+      } else if (current === findThisValue) {
+        // next = current.next;
+        newNode.next = current.next;
+        current.next = newNode;
+        return 'New node inserted';
       }
-      next = current.next;
-      current.next = newNode;
-      newNode.next = next;
-      return;
     }
     return 'No match found';
   }
