9ffe620026a31f4e2e393b33d8df3da614b6b648
schillerandrew <schiller.andrew@gmail.com>
2022-07-01 18:03:50 -0700

    new branch to trigger GitHub testing

---
+++ javascript/linked-list-kth/linked-list-kth.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList{
+  constructor(){
+    this.head = null;
+  }
+
+  kthFromEnd(k) {
+    if (!this.head) {
+      return 'There is no linked list to check.';
+    } else if (k < 0) {
+      return 'That is not a valid spot in the linked list!'
+    }
+    let current = this.head;
+    let count = 0;
+    let arrayifiedList = [];
+    while (current) {
+      arrayifiedList[count] = current.value;
+      current = current.next;
+      count++;
+    }
+    if (k >= arrayifiedList.length) {
+      return 'That is not a valid spot in the linked list!'
+    }
+    return arrayifiedList[arrayifiedList.length-k-1];
+  }
+}
+
+module.exports = {
+  Node,
+  LinkedList,
+};
